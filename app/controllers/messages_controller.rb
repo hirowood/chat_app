28f6675073e2c4166b68af753709711470be502c
hirowood <hirowoodandmika730@gmail.com>
2025-01-28 06:34:11 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = @tweet.comments.new(tweet_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
